2ae974757b461de4e3d37c26198a0aee8e8915be
Pranay Surve <pranaysurve75@gmail.com>
2022-07-24 17:24:23 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -122,7 +122,7 @@ export default function Home() {
                   <TextField variant='outlined' margin='normal' type="email" required fullWidth id='email' label='Email' name='email' onChange={handleInput} autoComplete='email' />
                 </Grid>
                 <Grid item={true} xs={12} sm={12} md={6} px={1}>
-                  <TextField  inputProps={{ inputMode: 'numeric',pattern: "[0-9]" }} 
+                  <TextField  inputProps={{ inputMode: 'numeric' }} 
                   // error={formValues.mobile === ""}
                   helperText={formValues.mobile === "" ? '10 Digits Mobile Number' : ' '}
                   variant='outlined' margin='normal' required fullWidth id='mobilenumber' label='Mobile Number' name='mobile' onChange={handleInput} />
